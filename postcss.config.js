eda8296bd5f383c3064e914be293c7d7291e77f5
rozklad <jan.rozklad@gmail.com>
2017-01-23 13:56:23 +0100

    installation fixes

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+/**
+ * Created by sane on 05/01/2017.
+ *
+ * Config for PostCSS
+ */
+
+module.exports = {
+  plugins: [
+    require('precss'),
+    require('postcss-assets')({
+      loadPaths: ['assets/'],
+      baseUrl: '/wp-content/themes/wyvern-child/'
+    }),
+    require('postcss-calc')
+  ]
+}
